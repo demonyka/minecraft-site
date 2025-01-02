3c1ae313129818c7ec8043225e694a2803363d34
demonyga <cweranit@gmail.com>
2025-01-02 19:14:11 +0400

    fix

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,28 +5,32 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request)
+    public function login(Request $request)
     {
-        $username = $request->input('username');
-        $password = $request->input('password');
+        $username = $request->input('Login');
+        $password = $request->input('Password');
+
+        if (!$username || !$password) {
+            return response()->json(["Message" => 'Все поля обязательны к заполнению'], 401);
+        }
 
         $user = User::where('username', $username)->first();
 
         if (!$user || !Hash::check($password, $user->password)) {
-            echo "Неверный логин или пароль";
-            return;
+            return response()->json(["Message" => "Неверный логин или пароль"], 401);
         }
 
-        if (!$user->whitelisted_until || $user->whitelisted_until < now()) {
-            echo "У Вас нет действующего пропуска";
-            return;
-        }
+//        if (!$user->whitelisted_until || $user->whitelisted_until < now()) {
+//            echo "У Вас нет действующего пропуска";
+//            return;
+//        }
 
-        echo "OK:$username";
+        return response()->json(["Login" => $user->username, "UserUuid" => $user->uuid, "Message" => "Успешная авторизация"]);
     }
 
 }
