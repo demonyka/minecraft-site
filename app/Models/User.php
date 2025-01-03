a479ecac713a2e3f135462c21a8eb0fa24e4e9f5
demonyga <cweranit@gmail.com>
2025-01-03 13:07:48 +0400

    Skins

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,19 +51,23 @@ class User extends Authenticatable
 
     public function uploadSkin(UploadedFile $file): void
     {
-        $mimeType = mime_content_type($file);
+        if ($file->getClientOriginalExtension() !== 'png') {
+            throw new \InvalidArgumentException('Файл должен быть в формате PNG.');
+        }
+
+        $mimeType = $file->getMimeType();
         if ($mimeType !== 'image/png') {
             throw new \InvalidArgumentException('Файл должен быть в формате PNG.');
         }
 
-        $imageSize = getimagesize($file);
+        $imageSize = getimagesize($file->getPathname());
         if (!($imageSize[0] === 64 && ($imageSize[1] === 32 || $imageSize[1] === 64))) {
             throw new \InvalidArgumentException('Размер изображения должен быть 64x32 или 64x64.');
         }
 
         $filePath = 'skins/' . $this->username . '/' . now()->timestamp . '.png';
 
-        Storage::disk('public')->put($filePath, file_get_contents($file));
+        Storage::disk('public')->put($filePath, file_get_contents($file->getPathname()));
 
         $this->skin_path = '/storage/' . $filePath;
         $this->save();
